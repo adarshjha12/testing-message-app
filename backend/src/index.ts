4420a9cd1c01b7dd8f5b5d0590b5a1d0dc2891f5
adarsh <jhaa50872@gmail.com>
2025-02-22 17:24:00 +0530

    Set up Express server with TypeScript, dotenv, and CORS configuration

---
+++ backend/src/index.ts
@@ -0,0 +1,13 @@
+const express = require('express')
+const dotenv = require('dotenv')
+const cors = require('cors')
+
+dotenv.config()
+const app = express()
+const port = process.env.PORT
+app.use(cors())
+
+app.listen(port, () =>{
+    console.log(`running on ${port}`);
+    
+})
